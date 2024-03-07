e41cfa500bd8081d72ae1b5de464f68312ba5934
De Oka <115537848+DeOkanee@users.noreply.github.com>
2024-03-07 09:47:50 +0800

    update

--- JS/header.js
+++ JS/header.js
@@ -1,14 +1,33 @@
-var prevScrollPos = window.pageYOffset;
 
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    var header = document.querySelector("header.fixed");
+function toggleDropdown() {
+  document.getElementById("kategoriDropdown").classList.toggle("show");
+}
 
-    if (prevScrollPos > currentScrollPos) {
-        header.style.top = "0";
-    } else {
-        header.style.top = "0"; /* Sesuaikan dengan tinggi header Anda */
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-toggle')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
+  }
+}
 
-    prevScrollPos = currentScrollPos;
-};
+//ini menuju dri header ke footer
+function scrollToFooter() {
+  var footer = document.querySelector("footer");
+  footer.scrollIntoView({ behavior: "smooth" });
+}
+
+//ini memunculkan contact person
+function toggleContactSection() {
+  var contactSection = document.getElementById("Contact");
+  if (contactSection.style.display === "none") {
+    contactSection.style.display = "block";
+  } else {
+    contactSection.style.display = "none";
+  }
+}
